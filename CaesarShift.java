1395f2d842e7b189446c09b509c0259334d7a6d9
Bizente1 <deleonbisente9@gmail.com>
2025-10-07 13:15:02 -0700

    I started the Shift

--- CaesarShift.java
+++ CaesarShift.java
@@ -1,1 +1,16 @@
 // TODO: Write your implementation to CaesarShift here!
+public class CaesarShift extends Substitution {
+    private int shift;
+
+    public CaesarShift(int shift){
+        super();
+        this.shift = shift;
+    }
+
+    public CaesarShift(int shift, String encoding){
+        super(encoding);
+        this.shift = shift;
+    }
+
+    
+}
